7bf0623a3c1b4db6dc9d2bf7268bcde271a78d2d
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-12-06 17:52:58 -0400

    update-list

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const GifExpertApp = () => {
+    const [categories, setcategories] = useState(['One puch', 'Dragon ball', 'Naruto'])
+    const handleAdd = () =>{
+        setcategories([
+            ...categories, 'pockemon go!'
+        ])
+    }
+
     return (
         <>
             <h2>GifExpertApp</h2> 
             <hr/>
+            <button onClick={handleAdd} >Agregar</button>
+            <ul>
+                {categories.map((item, index) => (
+                    <li key={index}>{item}</li>
+                )) }
+            </ul>
         </>
     )
 }
