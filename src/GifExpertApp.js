6e43a10db5d70e8c182ec05da9b032a21bfbaafe
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-12-06 23:00:26 -0400

    update

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,19 +1,21 @@
 import React, {useState} from 'react'
+import AddCategori from './components/AddCategori'
 
 const GifExpertApp = () => {
     const [categories, setcategories] = useState(['One puch', 'Dragon ball', 'Naruto'])
-    const [categories, setcategories] = useState(['One puch', 'Dragon ball', 'Naruto'])
-    const handleAdd = () =>{
-        setcategories([
-            ...categories, 'pockemon go!'
-        ])
-    }
+  
+    // const handleAdd = () =>{
+    //     setcategories([
+    //         ...categories, 'pockemon go!'
+    //     ])
+    // }
+
 
     return (
         <>
             <h2>GifExpertApp</h2> 
+            <AddCategori/>
             <hr/>
-            <button onClick={handleAdd} >Agregar</button>
             <ul>
                 {categories.map((item, index) => (
                     <li key={index}>{item}</li>
---
+++ src/components/AddCategori.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AddCategori = () => {
+    return (
+        <div>
+            <input
+                placeholder='Agregar'
+            />
+        </div>
+    )
+}
+
+export default AddCategori
